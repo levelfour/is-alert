42c866e9e003a387e14ea19fc32b7bf6d60bda61
levelfour <levelfourslv@gmail.com>
2015-04-09 00:08:49 +0900

    prepare admin page for homework_alert

--- alert/homework_alert/admin.py
+++ alert/homework_alert/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Homework
+from .models import User
+from .models import UserHomework
+
+admin.site.register(Homework)
+admin.site.register(User)
+admin.site.register(UserHomework)
