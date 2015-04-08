ace37bfc7e360e88a0c0472f2b53ed77a3deb99b
Fukumu Tsutsumi <levelfourslv@gmail.com>
2015-04-09 03:01:01 +0900

    fix homework_alert model

--- alert/homework_alert/models.py
+++ alert/homework_alert/models.py
@@ -3,15 +3,19 @@ from django.db import models
 
 class Homework(models.Model):
     lecture = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, null=True)
     deadline = models.DateTimeField('deadline')
+    created_at = models.DateTimeField('create datetime', null=True)
+    updated_at = models.DateTimeField('update datetime', null=True)
 
 
 class User(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=512)
-
+    created_at = models.DateTimeField('create datetime', null=True)
+    updated_at = models.DateTimeField('update datetime', null=True)
 
 class UserHomework(models.Model):
     user = models.ForeignKey(User)
     homework = models.ForeignKey(Homework)
-    created_at = models.DateTimeField('create datetime')
+    created_at = models.DateTimeField('create datetime', null=True)
