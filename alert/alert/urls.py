3e7cad535da59c82f0c8c371a64bf9c0ee65c7ab
Fukumu Tsutsumi <levelfourslv@gmail.com>
2015-04-09 23:07:00 +0900

    add signup view

--- alert/alert/urls.py
+++ alert/alert/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'alert.views.home', name='home'),
--- alert/homework_alert/views.py
+++ alert/homework_alert/views.py
@@ -5,3 +5,9 @@ from homework_alert.models import Homework
 def index(request):
     works = Homework.objects.all()
     return render(request, 'homework_alert/index.html', {'works': works})
+
+def login(request):
+    return render(request, 'homework_alert/login.html', {})
+
+def signup(request):
+    return render(request, 'homework_alert/signup.html', {})
