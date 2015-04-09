1d793ec55d80fc0d273843bf09f5b4d93da37a84
Fukumu Tsutsumi <levelfourslv@gmail.com>
2015-04-10 01:10:33 +0900

    implement user signup and login

--- alert/homework_alert/models.py
+++ alert/homework_alert/models.py
@@ -1,5 +1,15 @@
+import hashlib
 from django.db import models
 
+
+def get_nextautoincrement( mymodel ):
+    from django.db import connection
+    cursor = connection.cursor()
+    cursor.execute("SELECT seq FROM SQLITE_SEQUENCE WHERE name='%s';" % \
+                   mymodel._meta.db_table)
+    row = cursor.fetchone()
+    cursor.close()
+    return row[0]
 
 class Homework(models.Model):
     lecture = models.CharField(max_length=64)
@@ -11,9 +21,18 @@ class Homework(models.Model):
 
 class User(models.Model):
     email = models.EmailField()
-    password = models.CharField(max_length=512)
+    password = models.CharField(max_length=512, null=True)
     created_at = models.DateTimeField('create datetime', null=True)
     updated_at = models.DateTimeField('update datetime', null=True)
+    
+    @classmethod
+    def hash_password(cls, password_str):
+        salt = 'PdnzQnXf9RaB2m5QtnfubTTXLxL6HRKuY73fwbsZ'
+        return hashlib.sha256("{}{}{}".format(salt, int(get_nextautoincrement(cls))+1, password_str)).hexdigest()
+
+    def check_password(self, password_str):
+        salt = 'PdnzQnXf9RaB2m5QtnfubTTXLxL6HRKuY73fwbsZ'
+        return self.password == hashlib.sha256("{}{}{}".format(salt, self.id, password_str)).hexdigest()
 
 class UserHomework(models.Model):
     user = models.ForeignKey(User)
--- alert/homework_alert/views.py
+++ alert/homework_alert/views.py
@@ -1,5 +1,6 @@
+# coding: utf-8
 from django.shortcuts import render
-from homework_alert.models import Homework
+from homework_alert.models import User, Homework
 
 
 def index(request):
@@ -7,7 +8,33 @@ def index(request):
     return render(request, 'homework_alert/index.html', {'works': works})
 
 def login(request):
-    return render(request, 'homework_alert/login.html', {})
+    error = None
+    if request.method == 'POST':
+        if request.POST['mail'] == '':
+            error = 'メールアドレスを入力してください'
+        elif request.POST['password'] == '':
+            error = 'パスワードを入力してください'
+        else:
+            try:
+                user = User.objects.get(email=request.POST['mail'])
+                if user.check_password(request.POST['password']):
+                    return render(request, 'homework_alert/login.html', {'error': 'ログイン成功です'})
+                else:
+                    error = 'パスワードが違います'
+            except User.DoesNotExist as e:
+                error = '登録されていないメールアドレスです'
+    return render(request, 'homework_alert/login.html', {'error': error})
 
 def signup(request):
-    return render(request, 'homework_alert/signup.html', {})
+    error = None
+    if request.method == 'POST':
+        if request.POST['mail'] == '':
+            error = 'メールアドレスを入力してください'
+        elif request.POST['password'] == '' or request.POST['password2'] == '':
+            error = 'パスワードを入力してください'
+        elif request.POST['password'] != request.POST['password2']:
+            error = 'パスワードに誤りがあります'
+        else:
+            user = User.objects.create(email=request.POST['mail'], password=User.hash_password(request.POST['password']))
+            return render(request, 'homework_alert/signup_complete.html', {})
+    return render(request, 'homework_alert/signup.html', {'error': error})
